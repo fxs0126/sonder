be460f8331013a6bf36ae057e1ead5162c04fcc8
fanxs <fxs0126@sina.com>
2019-04-28 23:24:29 +0800

    [fanxs] sonder.h, message->messages

---
+++ src/include/sonder.h
@@ -0,0 +1,21 @@
+#ifndef _SONDER_H_
+#define _SONDER_H_
+
+#ifndef SONDER_DLL_DECL
+#	ifdef WIN32 
+# 		define SONDER_DLL_DECL __declspec(dllimport)
+#	else 
+# 		define SONDER_DLL_DECL 
+#	endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif 
+
+ 
+#ifdef __cplusplus
+} //extern "C"
+#endif 
+
+#endif
