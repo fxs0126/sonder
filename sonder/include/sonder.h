8a5bb6799a7699326dfcc063eb617e541458bc32
fanxs <fxs0126@sina.com>
2019-05-04 16:40:25 +0800

    [fanxs] 2019.05.04

--- sonder/include/sonder.h
+++ sonder/include/sonder.h
@@ -1,6 +1,8 @@
 #ifndef _SONDER_H_
 #define _SONDER_H_
 
+
+
 #ifndef SONDER_DLL_DECL
 #	ifdef WIN32 
 # 		define SONDER_DLL_DECL __declspec(dllimport)
@@ -13,8 +15,29 @@
 extern "C" {
 #endif 
 
+typedef struct t_sonder_value {
+	unsigned char format;
+	char*	      pUnits;
+	union {
+		void* pHandle;
+		int   integer;
+		double real;
+		struct t_sonder_vecval*  vertor;
+	};	
+} t_sonder_value,*p_sonder_value;
 
- 
+typedef struct t_sonder_vecval {
+	unsigned int count;
+	struct t_sonder_value* vecval;
+}t_sonder_vecval,*p_sonder_vecval;
+
+#define SONDER_H_INTERAL 1
+#include "../version/version.h"
+#include "../common/common.h"
+#include "../messages/messages.h"
+#include "../options/options.h"
+#undef SONDER_H_INTERAL
+
 #ifdef __cplusplus
 } //extern "C"
 #endif 
